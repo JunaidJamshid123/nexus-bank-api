23f16ac03555432dce5fb3a05c67337f34356c2e
JUNAIDJAMSHID <junaidjamshid@carecloud.com>
2026-05-19 16:06:04 +0500

    rate limit max request increased

--- src/middlewares/rateLimiter.middleware.js
+++ src/middlewares/rateLimiter.middleware.js
@@ -11,15 +11,15 @@ const createLimiter = (windowMs, max, message) => {
 };
 
 // Auth - public endpoints (stricter)
-const authLimiter = createLimiter(60 * 1000, 5, 'Too many attempts, please try again after 1 minute');
+const authLimiter = createLimiter(60 * 1000, 10, 'Too many attempts, please try again after 1 minute');
 
 // Login / MPIN reset - very strict
-const loginLimiter = createLimiter(60 * 1000, 4, 'Too many login attempts, please try again after 1 minute');
+const loginLimiter = createLimiter(60 * 1000, 10, 'Too many login attempts, please try again after 1 minute');
 
 // Protected endpoints - general
-const apiLimiter = createLimiter(60 * 1000, 5, 'Too many requests, please try again after 1 minute');
+const apiLimiter = createLimiter(60 * 1000, 12, 'Too many requests, please try again after 1 minute');
 
 // Sensitive mutations (password/mpin change, delete)
-const sensitiveLimiter = createLimiter(60 * 1000, 4, 'Too many requests, please try again after 1 minute');
+const sensitiveLimiter = createLimiter(60 * 1000, 10, 'Too many requests, please try again after 1 minute');
 
 module.exports = { authLimiter, loginLimiter, apiLimiter, sensitiveLimiter };
